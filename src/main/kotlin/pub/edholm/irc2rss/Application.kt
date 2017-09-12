2066550d332c34c9fa0c657a2ac29ba798c51131
Emil Edholm <bobby@verypublic.org>
2017-09-12 22:35:49 +0200

    Remove releases older than 72h

--- src/main/kotlin/pub/edholm/irc2rss/Application.kt
+++ src/main/kotlin/pub/edholm/irc2rss/Application.kt
@@ -2,12 +2,12 @@ package pub.edholm.irc2rss
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableMongoRepositories
-@EnableMongoAuditing
+@EnableScheduling
 class Application {
 }
 
---
+++ src/main/kotlin/pub/edholm/irc2rss/ScheduledRemover.kt
@@ -0,0 +1,33 @@
+package pub.edholm.irc2rss
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import pub.edholm.irc2rss.database.ReleaseRepository
+import java.time.Duration
+import java.time.Instant
+
+@Component
+class ScheduledRemover(private val releaseRepository: ReleaseRepository) {
+  companion object {
+    const val MAXIMUM_AGE_IN_SECONDS = 72 * 60 * 60L
+  }
+
+  val log: Logger = LoggerFactory.getLogger(ScheduledRemover::class.java)
+
+  @Scheduled(initialDelay = 10000L, fixedDelay = 172800000)
+  fun removeOldReleases() {
+    val releasesTooOld = releaseRepository
+      .findAll()
+      .filter {
+        Duration.between(it.datePublished, Instant.now())
+          .abs()
+          .seconds > MAXIMUM_AGE_IN_SECONDS
+      }
+
+    log.info("Removing ${releasesTooOld.size} releases that are older than 72h")
+    log.debug("Removing the following releases: $releasesTooOld")
+    releaseRepository.deleteAll(releasesTooOld)
+  }
+}
