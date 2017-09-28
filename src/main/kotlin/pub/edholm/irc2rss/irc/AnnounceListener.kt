c74c05ff312c4aa1ba178e5f2e379f3685c94fb1
Emil Edholm <bobby@verypublic.org>
2017-09-28 20:21:17 +0200

    Add some debug logging

--- src/main/kotlin/pub/edholm/irc2rss/irc/AnnounceListener.kt
+++ src/main/kotlin/pub/edholm/irc2rss/irc/AnnounceListener.kt
@@ -3,6 +3,8 @@ package pub.edholm.irc2rss.irc
 import org.pircbotx.Colors
 import org.pircbotx.hooks.ListenerAdapter
 import org.pircbotx.hooks.events.MessageEvent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import pub.edholm.irc2rss.Properties
 import pub.edholm.irc2rss.database.ReleaseRepository
@@ -12,16 +14,21 @@ import pub.edholm.irc2rss.domain.Release
 
 @Component
 class AnnounceListener(private val releaseRepository: ReleaseRepository,
-                       private val properties: Properties) : ListenerAdapter() {
+                       private val properties: Properties,
+                       private val logger: Logger = LoggerFactory.getLogger(AnnounceListener::class.java)) : ListenerAdapter() {
   companion object {
     val announceRegex = Regex("^New Torrent Announcement:\\s*<([^>]*)>\\s*Name:'(.*)' uploaded by '([^']*)'\\s*-\\s*https?\\:\\/\\/([^\\/]+\\/)torrent/(\\d+)")
   }
 
   override fun onMessage(event: MessageEvent) {
+    logger.debug("Received message: " + event.message)
     val parts = splitAnnouncement(event.message) ?: return
+    logger.debug("Split parts: " + parts)
 
     val link = constructDownloadLink(parts)
     val release = Release(title = parts.title, category = parts.category, torrentId = parts.torrentId, link = link)
+
+    logger.debug("Parsed release: " + release)
     releaseRepository.save(release)
   }
 
